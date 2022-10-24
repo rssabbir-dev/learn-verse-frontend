b34a64e7e7c1b1b391ea559fa38f81d05ab1c203
rssabbir <sabbirhossainrahad@gmail.com>
2022-10-24 23:04:18 +0600

    instail react-router-dom and set PrivateRoute

---
+++ src/routes/PrivateRoute.js
@@ -0,0 +1,17 @@
+import React, { useContext } from 'react';
+import { Navigate, useLocation } from 'react-router-dom';
+import { AuthContext } from '../context/AuthProvider/AuthProvider';
+
+const PrivateRoute = ({ children }) => {
+    const location = useLocation();
+    const { user, loading } = useContext(AuthContext);
+    if (loading) {
+        return <h3 className='text-3xl'>Loading....</h3>
+    }
+    if (!user?.uid) {
+        return <Navigate to='/login' state={{from:location}} replace />
+    }
+    return children;
+};
+
+export default PrivateRoute;
